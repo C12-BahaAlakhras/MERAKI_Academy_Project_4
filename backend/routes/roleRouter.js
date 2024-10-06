2e138fbaed6b1f676bd6fb9e8f0e7fa7c2ab2fef
BahaAlakhras <alakhras.baha@gmail.com>
2024-10-06 19:03:04 +0300

    create auth & authoriz

--- backend/routes/roleRouter.js
+++ backend/routes/roleRouter.js
@@ -1,18 +1,16 @@
 const express = require("express");
-const { createNewRole } = require("../controllers/rolesControllers");
+const { createNewRole } = require("../controllers/roleControllers");
 //
 const rolesRouter = express.Router();
 
-
 rolesRouter.post("/", createNewRole);
-
 
 module.exports = rolesRouter;
 
 /*
  * Testing Routes:
  * POST -> http://localhost:5000/roles/
-*/
+ */
 
 /*
 add roles
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -10,7 +10,6 @@ const {
 //
 const usersRouter = express.Router();
 
-
 // ! POST
 // register free plan -> http://localhost:5000/user/register/admin
 usersRouter.post("/register/admin", createNewUserAdmin);
@@ -30,20 +29,8 @@ usersRouter.get("/:projectID/:ticketId", allUsersInTicket);
 // get user by id -> http://localhost:5000/user/:userID
 usersRouter.get("/:userID", getUsersById);
 
-
 module.exports = usersRouter;
 
 /*
-add roles
-{
-  "role": "USER",
-  "permissions": ["USER"]
-}
-{
-  "role": "ADMIN",
-  "permissions": ["ADMIN"]
-}
 
-USER PERMISSION = [ "ASSIGN_TEAM", "CREATE_CARDS", "CREATE_COMMENTS"]
-ADMIN PERMISSION = ["CREATE_PROJECT" , "CREATE_TICKET" , "ADD_TEAM", "CREATE_CARDS", "CREATE_COMMENTS"]
 */
